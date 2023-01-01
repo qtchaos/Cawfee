00db10b0abc12ebec945d74615925f6b95fb8852
chaos <72168435+qtchaos@users.noreply.github.com>
2023-01-01 03:23:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.plugin import choose_plugins, load_plugins
 
 LOGGER = Logger()
 
-
+x = {  "a": "b"      }
 def main():
     LOGGER.log("Brewing a fresh cup of coffee...")
     if not os.path.exists("config.json"):
